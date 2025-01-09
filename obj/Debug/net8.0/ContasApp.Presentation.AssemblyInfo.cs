c1eb2631303facb7b7673196f211c6573130fbf2
Reisanderson146 <reisanderson146@gmail.com>
2025-01-09 20:10:06 -0300

    feat: Desenvolvimento do repositório de usuários

--- obj/Debug/net8.0/ContasApp.Presentation.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContasApp.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContasApp.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a9d5625351bc16b74521642169743896e42616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e107ec7850239e28862b79ddec0402914d1bdbc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContasApp.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContasApp.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
